f4fc6a63b130d1aa43886cd9c01064e03ccfac1c
verycumbersome <matthewsages@gmail.com>
2017-09-03 18:50:59 -0700

    Add scroll down

--- MatthewJordanApp/static/javascript/main.js
+++ MatthewJordanApp/static/javascript/main.js
@@ -1,5 +1,20 @@
-window.onload = function() {
-2   Particles.init({
-3     selector: '#particles-js'
-4   });
-5 };
+$(document).ready(function() {
+    var page = $("html, body");
+
+    setInterval(function(){
+        $(".scrolldownarrow").stop().effect("bounce", { times:3, distance:20, easing:"easeInCubic" }, 'normal');
+    }, 1500);
+
+    $(".scrolldown").click(function(e) {
+
+        page.on("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function(){
+           page.stop();
+        });
+
+        page.stop().animate({scrollTop: $("#resume").position().top}, 1500, function(){
+           page.off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
+        });
+
+       return false;
+    });
+});
