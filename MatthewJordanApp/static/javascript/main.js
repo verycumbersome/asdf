54099014aadca21f65e9a11b75719eba514d16d0
verycumbersome <matthewsages@gmail.com>
2017-09-03 18:52:42 -0700

    Add scrolldown

--- MatthewJordanApp/static/javascript/main.js
+++ MatthewJordanApp/static/javascript/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     var page = $("html, body");
 
+    $("html,body").animate({scrollTop: 0}, 100);
+
     setInterval(function(){
         $(".scrolldownarrow").stop().effect("bounce", { times:3, distance:20, easing:"easeInCubic" }, 'normal');
     }, 1500);
@@ -11,7 +13,7 @@ $(document).ready(function() {
            page.stop();
         });
 
-        page.stop().animate({scrollTop: $("#resume").position().top}, 1500, function(){
+        page.stop().animate({scrollTop: $("#about-me").position().top}, 1500, function(){
            page.off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
         });
 
